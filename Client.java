b42899ef3900fd985a554b47652fee3485cc8914
sdthomas92 <sdthomas92@gmail.com>
2015-11-19 00:11:14 -0800

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,6 +1,7 @@
 import java.lang.reflect.Array;
 import java.net.*;
 import java.io.*;
+import java.nio.charset.Charset;
 import java.util.*;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
@@ -10,13 +11,14 @@ import javax.swing.SwingUtilities;
  * The Client that can be run both as a console or a GUI
  */
 public class Client {
+        private static final String key = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 	private ObjectInputStream	sInput;	// to read from the socket
 	private ObjectOutputStream	sOutput;	// to write on the socket
-	private Socket				socket;
-	private ClientGUI			clientGUI;
-	private String				server, username;
-	private int					port;
-	private int					attempt, userMaxAttempt;
+	private Socket			socket;
+	private ClientGUI		clientGUI;
+	private String			server, username;
+	private int			port;
+	private int			attempt, userMaxAttempt;
 	/*
 	 * Constructor called by console mode server: the server address port: the port number username: the username
 	 */
@@ -39,34 +41,36 @@ public class Client {
 		this(server, port, null, null, attempts);
 	}
 	public boolean start() {
-		// try to connect to the server
-		try {
-			socket = new Socket(server, port);
-		} catch (Exception e) {
-			displayToClientScreen("Cannot establish a connection to server: " + server + " on port: " + port + "\n" + e);
-			return false;
-		}
-		String msg = "Connection accepted " + socket.getInetAddress() + ":" + socket.getPort();
-		displayToClientScreen(msg);
-		/* Creating both Data Stream */
-		try {
-			sInput = new ObjectInputStream(socket.getInputStream());
-			sOutput = new ObjectOutputStream(socket.getOutputStream());
-		} catch (IOException e) {
-			displayToClientScreen("Error. Data stream could not be establish.\n" + e);
-			return false;
-		}
-		// creates the Thread to listen from the server
-		new ListenFromServer().start();
-		try {
-			sOutput.writeObject(username);
-		} catch (IOException er) {
-			displayToClientScreen("Exception doing login : " + er);
-			disconnect();
-			return false;
-		}
-		// success we inform the caller that it worked
-		return true;
+            // try to connect to the server
+            try {
+                socket = new Socket(server, port);
+            } catch (Exception e) {
+		displayToClientScreen("Cannot establish a connection to server: " + 
+                                      server + " on port: " + port + "\n" + e);
+		return false;
+            }
+            String msg = "Connection accepted " + socket.getInetAddress() + 
+                         ":" + socket.getPort();
+            displayToClientScreen(msg);
+            /* Creating both Data Stream */
+            try {
+                sInput = new ObjectInputStream(socket.getInputStream());
+		sOutput = new ObjectOutputStream(socket.getOutputStream());
+            } catch (IOException e) {
+		displayToClientScreen("Error. Data stream could not be establish.\n" + e);
+		return false;
+            }
+            // creates the Thread to listen from the server
+            new ListenFromServer().start();
+            try {
+                sOutput.writeObject(username);
+            } catch (IOException er) {
+		displayToClientScreen("Exception doing login : " + er);
+		disconnect();
+		return false;
+            }
+            // success we inform the caller that it worked
+            return true;
 	}
 	/*
 	 * To send a message to the console or the GUI
@@ -76,58 +80,35 @@ public class Client {
 		else clientGUI.displayToClientScreen("\n" + msg); // append to the ClientGUI display field.
 	}
 	public void uploadFile() {
-		try {
-			String fileName = null;
-			JFileChooser fc = new JFileChooser();
-			if (fc.showSaveDialog(null) != JFileChooser.CANCEL_OPTION) fileName = fc.getSelectedFile().getAbsolutePath();
-			else return;
-			File file = new File(fileName);
-			// send file
-			byte[] fileArray = new byte[(int) file.length()];
-			if (fileArray.length > Math.pow(2, 20)) {
-				JOptionPane.showMessageDialog(null, "Na cant send file too big." + fileArray.length + ". File has to be less than or equal to 1MB.\nYou have "
-				+ (this.getMaxAttemptAllowed() - this.getAttempt() + " attempts remaining."));
-				this.fileTransferUnsuccessful();
-				if (this.getAttempt() > this.getMaxAttemptAllowed()) {
-					JOptionPane
-					.showMessageDialog(null, "You have exceeded the maximum allowed attempts. Your connection with the server will be disconnected.");
-					disconnect();
-				}
-				return;
-			}
-			sendBytes(file, fileArray);
-		} catch (Exception e) {
-			e.printStackTrace();
+            try {
+                System.out.println("UPLOADING...");
+                String fileName = null;
+		JFileChooser fc = new JFileChooser();
+		if (fc.showSaveDialog(null) != JFileChooser.CANCEL_OPTION) 
+                    fileName = fc.getSelectedFile().getAbsolutePath();
+		else 
+                    return;
+		File file = new File(fileName);
+		// send file
+		byte[] fileArray = new byte[(int) file.length()];
+		if (fileArray.length > Math.pow(2, 20)) {
+                    JOptionPane.showMessageDialog(null, 
+                                                  "Na cant send file too big." + fileArray.length + ". File has to be less than or equal to 1MB.\nYou have " +
+				                  (this.getMaxAttemptAllowed() - this.getAttempt() + " attempts remaining."));
+                    this.fileTransferUnsuccessful();
+                    if (this.getAttempt() > this.getMaxAttemptAllowed()) {
+			JOptionPane.showMessageDialog(null, 
+                                                      "You have exceeded the maximum allowed attempts. Your connection with the server will be disconnected.");
+			disconnect();
+                    }
+                    return;
 		}
+                sendFile(file, FTP.keyFile);
+            } catch (Exception e) {
+		e.printStackTrace();
+            }
 	}
-	private void sendBytes(File file, byte[] fileArray) {
-		try {
-			BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
-			// send 1KB at a time
-			bis.read(fileArray, 0, fileArray.length);
-			int remaining = 0;
-			System.out.println("size: "+fileArray.length);
-			for ( int i = 0 ; i < fileArray.length ; i += Math.pow(2, 10)) {
-				System.out.println("i:: "+i);
-				byte[] a = new byte[(int) Math.pow(2, 10)];
-				
-				a = Arrays.copyOfRange(fileArray, i, i + (int) Math.pow(2, 10));
-				if (a.length <= 0) break; // this will happen if the bounds i to 2^20 exceeds fileArray's bounds
-				else this.sOutput.writeObject(new NetworkMessage(NetworkMessage.UPLOADFILE, a));
-				remaining = i;
-			}
-			// send remaining bytes
-			byte[] r = new byte[(int) Math.pow(2, 10)];
-			for ( int i = fileArray.length - remaining ; i < r.length ; ++i) {
-				if (i > fileArray.length) r[i] = 0;// padding
-				else r[i] = fileArray[i];// send remaining bytes
-			}
-			this.sOutput.writeObject(new NetworkMessage(NetworkMessage.LASTPACKETSENT, r));
-		} catch (Exception e) {// just sending bytes
-			this.displayToClientScreen("> Unsuccessful file transfer.");
-			this.disconnect();
-		}
-	}
+        
 	public int getMaxAttemptAllowed() {
 		return this.userMaxAttempt;
 	}
@@ -137,7 +118,7 @@ public class Client {
 	void sendMessage(NetworkMessage msg) {
 		try {
 			sOutput.writeObject(msg);
-		} catch (IOException e) {
+		} catch (Exception e) {
 			displayToClientScreen("Exception writing to server: " + e);
 		}
 	}
@@ -146,13 +127,13 @@ public class Client {
 	 */
 	public void disconnect() {
 		try {
-			if (sInput != null) sInput.close();
+                    if (sInput != null) sInput.close();
+		} catch (Exception e) {}
+                try {
+                    if (sOutput != null) sOutput.close();
 		} catch (Exception e) {}
 		try {
-			if (sOutput != null) sOutput.close();
-		} catch (Exception e) {}
-		try {
-			if (socket != null) socket.close();
+                    if (socket != null) socket.close();
 		} catch (Exception e) {}
 		if (clientGUI != null) clientGUI.connectionFailed();
 	}
@@ -166,25 +147,77 @@ public class Client {
 	 ** MAIN LOOP THAT SERVER LISTENS TO CLIENT
 	 ****/
 	private class ListenFromServer extends Thread {
-		public void run() {
-			while (true) {
-				try {
-					String msg = (String) sInput.readObject();
-					if (clientGUI == null) {
-						System.out.println(msg);
-						System.out.print("> ");
-					} else {
-						clientGUI.displayToClientScreen("\n> " + msg);
-					}
-				} catch (IOException e) {
-					displayToClientScreen("\n> Connection with the server has been terminated.");
-					if (clientGUI != null) clientGUI.connectionFailed();
-					else disconnect();
-					break;
-				} catch (ClassNotFoundException e) {
-					e.printStackTrace();
-				}
+            public void run() {
+                while (true) {
+                    try {
+                        String msg = (String) sInput.readObject();
+			if (clientGUI == null) {
+                            System.out.println(msg);
+                            System.out.print("> ");
+			} 
+                        else {
+                            clientGUI.displayToClientScreen("\n> " + msg);
 			}
+                    } catch (IOException e) {
+			displayToClientScreen("\n> Connection with the server has been terminated.");
+			if (clientGUI != null) 
+                            clientGUI.connectionFailed();
+			else disconnect();
+                            break;
+                    } catch (ClassNotFoundException e) {
+			e.printStackTrace();
+                    }
 		}
+            }
 	}
+    public void sendFile(File inputFile, File keyFile) {       
+        byte[] file, key;
+        try {
+            //Converts both the inputFile and keyFile into byte arrays.
+            file = FTP.fileToByteArray(inputFile);
+            key = FTP.fileToByteArray(keyFile);
+            
+            //For every kilobyte in the file, a chunk is made, hashed, and these
+            //are both merged into a byte array of length 1028, encrypted, encoded 
+            //in Base 64, and sent.
+            for(int i = 0; i < file.length; i += 1024) {
+                //Creates a kilobyte byte array for the current file chunk. If
+                //this is the last iteration and this file chunk is less than a
+                //kilobyte in length (the remander of the file), the byte array
+                //length is adjusted accordingly.
+                byte[] send = new byte[(file.length - i) < 1024 ? (file.length - i) + 4 : 1028];
+                
+                //The new byte array is filled with the chunk's contents.
+                for(int j = 0; j < send.length-4; j++)
+                    send[j] = file[i+j];
+               
+                //The hash is generated (4 bytes in length).
+                byte[] hash = FTP.hash(send, key);
+                
+                //The hash is put in the last remaining 4 bytes of the byte array to be sent.
+                for(int j = 0; j < hash.length; j++)
+                    send[((file.length - i) < 1024 ? (file.length - i): 1024) + j] = hash[j];
+                
+                //The byte array is encrypted.
+                byte[] encrypt = FTP.encryptDecrypt(send, key);
+
+                //The byte array is encoded in Base64
+                String encode = FTP.encodeBase64(encrypt);
+
+                //The encoded string is converted into a byte array representation. This
+                //is what will be sent to the receiver.
+                byte[] finalArray = encode.getBytes(Charset.forName("UTF-8"));
+
+                //finalArray is sent to the receiver.
+                this.sOutput.writeObject(new NetworkMessage(NetworkMessage.UPLOADFILE, finalArray));
+                
+            }
+            this.sOutput.writeObject(new NetworkMessage(NetworkMessage.LASTPACKETSENT, 
+                                                        FTP.encryptDecrypt(inputFile.getName().getBytes(Charset.forName("UTF-8")), 
+                                                                           key)));
+        }
+        catch (Exception e){
+            System.out.println("File or key don't exist.");
+        }      
+    }
 }
