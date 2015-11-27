7cf4a2fe0c9bbcdadf5f6cc0259c64cc5558f3ee
sdthomas92 <sdthomas92@gmail.com>
2015-11-27 15:30:31 -0800

    ServerGUI.java

---
+++ ServerGUI.java
@@ -0,0 +1,135 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/*
+ * The server as a GUI
+ */
+public class ServerGUI extends JFrame implements ActionListener, WindowListener {
+	private static final long	serialVersionUID	= 1L;
+	// the stop and start buttons
+	private JButton				connectButton, addKey;
+	private JTextArea			serverTextField, event;
+	// The port number
+	private JTextField			tPortNumber;
+	// my server
+	private Server				server;
+	private boolean				inBG;
+        private JLabel asciiLabel;
+        
+	// server constructor that receive the port to listen to for connection as parameter
+	ServerGUI(int port, boolean visible) {
+		super("Server");
+		server = new Server(port);
+		inBG = !visible;
+		new ServerRunning().start();
+		setVisible(false);
+	}
+	ServerGUI(int port) {
+		super("Server");
+		// server = null;
+		// in the NorthPanel the PortNumber the Start and Stop buttons
+		JPanel north = new JPanel();
+		north.add(new JLabel("Port number: "));
+		tPortNumber = new JTextField("" + port);
+		north.add(tPortNumber);
+		// to stop or start the server, we start with "Start"
+		connectButton = new JButton("Start");
+		connectButton.addActionListener(this);
+                connectButton.setEnabled(false);
+		addKey = new JButton("Select Key");
+		addKey.addActionListener(this);
+                asciiLabel = new JLabel("ASCII armor");
+                server.asciiArmored = true;
+                
+		north.add(connectButton);
+                north.add(addKey);
+                north.add(asciiLabel);
+		add(north, BorderLayout.NORTH);
+		JPanel center = new JPanel(new GridLayout(2, 1));
+		serverTextField = new JTextArea(80, 80);
+		serverTextField.setEditable(false);
+		displayServerScreen("> Server is not currently listening.");
+		center.add(new JScrollPane(serverTextField));
+		event = new JTextArea(80, 80);
+		event.setEditable(false);
+		displayToEventLog("Events log.");
+		center.add(new JScrollPane(event));
+		add(center);
+		addWindowListener(this);
+		setSize(450, 600);
+		this.setLocationRelativeTo(null);
+		setVisible(true);
+	}
+	// append message to the two JTextArea
+	// position at the end
+	void displayServerScreen(String str) {
+		serverTextField.append(str + "\n");
+	}
+	void displayToEventLog(String str) {
+		event.append(str + "\n");
+	}
+	// start or stop where clicked
+	public void actionPerformed(ActionEvent e) {
+		Object object = e.getSource();
+                if (object == addKey) {
+                    FTP.uploadKey();
+                    connectButton.setEnabled(true);
+                }
+                if (e.getSource() == this.connectButton && this.connectButton.getText().equalsIgnoreCase("Stop") && server != null) {
+			server.disconnectClients();
+			server = null;
+			tPortNumber.setEditable(true);
+			connectButton.setText("Start");
+		} else {
+			// OK start the server
+			int port;
+			try {
+				port = Integer.parseInt(tPortNumber.getText().trim());
+			} catch (Exception ee) {
+				displayToEventLog("Invalid port number.");
+				return;
+			}
+			// create a new Server
+			server = new Server(port, this);
+			// and start it as a thread
+			new ServerRunning().start();
+			this.displayServerScreen("> Server is listening on port: " + port);
+			connectButton.setText("Stop");
+			tPortNumber.setEditable(false);
+		}
+	}
+	/*
+	 * If the user click the X button to close the application connection with the server will be released and port freed.
+	 */
+	public void windowClosing(WindowEvent e) {
+		if (server != null) {
+			try {
+				server.disconnectClients(); // close the connection
+			} catch (Exception ee) {
+				ee.printStackTrace();
+			}
+			server = null;
+		}
+		FTP.main(null);
+	}
+        
+	public void windowClosed(WindowEvent e) {}
+	public void windowOpened(WindowEvent e) {}
+	public void windowIconified(WindowEvent e) {}
+	public void windowDeiconified(WindowEvent e) {}
+	public void windowActivated(WindowEvent e) {}
+	public void windowDeactivated(WindowEvent e) {}
+	/*
+	 * A thread to run the Server
+	 */
+	class ServerRunning extends Thread {
+		public void run() {
+			if (inBG) server.startServer();
+			else {
+				server.startServer();
+				// if (inBG) appendEvent("Server crashed.");
+			}
+		}
+	}
+}
